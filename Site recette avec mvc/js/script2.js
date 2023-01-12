cc8502ce2350e4a849fe382390d1948d03fcce0c
Contino Maxime <91802050+MaxCtn@users.noreply.github.com>
2023-01-12 17:20:21 +0100

    Add files via upload

--- Site recette avec mvc/js/script2.js
+++ Site recette avec mvc/js/script2.js
@@ -1,17 +1,3 @@
-function openLoginPage() {
-    document.querySelector(".reg").classList.remove("show-page");
-    document.querySelector(".login").classList.add("show-page");
-    document.getElementById("login-action").classList.add("show");
-    document.getElementById("reg-action").classList.remove("show");
-}
-
-function openRegPage() {
-    document.querySelector(".reg").classList.add("show-page");
-    document.querySelector(".login").classList.remove("show-page");
-    document.getElementById("reg-action").classList.add("show");
-    document.getElementById("login-action").classList.remove("show");
-}
-
 $(document).ready(function() {
     var zindex = 10;
 
