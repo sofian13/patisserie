c7483169494ae24b31d7e1733170d3bfdab8c8b6
Contino Maxime <91802050+MaxCtn@users.noreply.github.com>
2023-01-13 11:28:15 +0100

    Add files via upload

--- site_recette_avec_mvc/Vues/login/voir.php
+++ site_recette_avec_mvc/Vues/login/voir.php
@@ -8,6 +8,9 @@
         <span class="load" onclick="openRegPage()" id="reg-action"
           >Inscription</span
         >
+        
+        
+</div>
       </div>
       <div class="login show-page">
         <input type="text" placeholder="Pseudo" />
@@ -15,14 +18,23 @@
         <button>Connexion</button>
         <a href="#" onclick="openRegPage()">Inscription</a>
       </div>
+  <form class="form">
       <div class="reg">
-        <input type="text" placeholder="Pseudo" />
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Mot de passe" />
-        <button>Inscription</button>
-        <a href="#" onclick="openLoginPage()">Connexion</a>
+        <div class="avatar-input">
+          <input type="file" name="avatar" id="avatar" accept="image/*">
+          <label for="avatar">Choose an avatar</label>
+          <img src="https://media.discordapp.net/attachments/885515822817234954/1063397423428403230/pngegg.png?width=581&height=581" id="avatar-preview" alt="avatar">
+          <input type="text" placeholder="Pseudo" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Mot de passe" />
+          <input type="password" placeholder="Confirmer Mot de passe" />
+          <button>Inscription</button>
+          <a href="#" onclick="openLoginPage()">Connexion</a>
+        </div>
       </div>
     </form>
+
+  </form>
   </body>
   <script src="script.js"  ></script>  
 </html>
