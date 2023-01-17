ab7aefe73d9961adad2fb126cac44f22f8d27e5a
LiciuStefan <89767945+LiciuStefan@users.noreply.github.com>
2023-01-17 16:49:18 +0100

    Add files via upload

--- site_recette_avec_mvc/Controleurs/ControleurRecherche.php
+++ site_recette_avec_mvc/Controleurs/ControleurRecherche.php
@@ -9,11 +9,17 @@ final class ControleurRecherche
 
     }
 
-    public function testformAction(Array $A_parametres = null, Array $A_postParams = null)
-    {
+        //Fonction de recherche des recettes
+    public function searchRecipes($query) {
+        $host = 'mysql-thesavorist.alwaysdata.net  ';
+        $dbname = 'thesavorist_site';
+        $username = '295285';
+        $password = '*OnadesnotesIncr13*';
 
-        Vue::montrer('recherche/testform', array('formData' =>  $A_postParams));
-
+        $db = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+        $stmt = $this->$db->prepare("SELECT id, nom FROM recettes WHERE nom LIKE '%$query%'");
+        $stmt->execute();
+        return $stmt->fetchAll();
     }
 
 }
