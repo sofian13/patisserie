a569efed46fed355f415455b2e1584d78d6be4ff
LiciuStefan <89767945+LiciuStefan@users.noreply.github.com>
2023-01-17 16:46:28 +0100

    Add files via upload

--- site_recette_avec_mvc/Vues/recherche/voir.php
+++ site_recette_avec_mvc/Vues/recherche/voir.php
@@ -1,5 +1,17 @@
  <!-- Formulaire de recherche -->
- <form method="get" action="search.php">
+ <form method="get" action="index.php?url=Recherche/searchRecipes">
     <input type="text" name="query" placeholder="Rechercher une recette">
     <input type="submit" value="Rechercher">
 </form>
+
+<?php 
+
+$recherche = new Recherche();
+if(isset($_GET['query'])){
+    $query = $_GET['query'];
+    $recipes = $recherche->searchRecipes($query);
+    foreach ($recipes as $recipe) {
+        echo '<li><a href="recipe.php?id=' . $recipe['id'] . '">' . $recipe['nom'] . '</a></li>';
+    }
+}
+?>
