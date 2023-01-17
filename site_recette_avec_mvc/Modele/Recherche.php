5c849bbb734e4b49188648965c53f4c3ad4ed6b1
LiciuStefan <89767945+LiciuStefan@users.noreply.github.com>
2023-01-17 10:10:12 +0100

    Add files via upload

--- site_recette_avec_mvc/Modele/Recherche.php
+++ site_recette_avec_mvc/Modele/Recherche.php
@@ -3,14 +3,20 @@
 final class Recherche
 {
     private $_S_message = "Recherche";
-
+    
     public function donneMessage()
     {
         return $this->_S_message ;
     }
   //Fonction de recherche des recettes
   public function searchRecipes($query) {
-    $stmt = $this->db->prepare("SELECT id, nom FROM recettes WHERE nom LIKE '%$query%'");
+    $host = 'mysql-thesavorist.alwaysdata.net  ';
+        $dbname = 'thesavorist_site';
+        $username = '295285';
+        $password = '*OnadesnotesIncr13*';
+
+    $db = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $stmt = $this->$db->prepare("SELECT id, nom FROM recettes WHERE nom LIKE '%$query%'");
     $stmt->execute();
     return $stmt->fetchAll();
   }
