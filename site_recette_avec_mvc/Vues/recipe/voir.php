fe55801d144450c7dd71d8687b3dc3a248bbeef7
Contino Maxime <91802050+MaxCtn@users.noreply.github.com>
2023-01-17 16:35:59 +0100

    page recette

---
+++ site_recette_avec_mvc/Vues/recipe/voir.php
@@ -0,0 +1,97 @@
+<div class="w-3/5 mx-auto rounded shadow-lg p-8 bg-white flex flex-col gap-8">
+  <ul class="text-sm text-primary flex gap-8 font-black">
+    <li>Brownies au noix de pécan</li>
+  </ul>
+
+  <div>
+    <h1 class="text-4xl font-black text-center">Brownies au noix de pécan</h1>
+  </div>
+
+  <div class="w-3/4 mx-auto">
+    <div class="rounded-lg bg-black mx-auto">
+      <img src="https://picsum.photos/400" class="block mx-auto w-1/2" />
+    </div>
+    <ul class="flex gap-4 overflow-auto mt-4 pb-4">
+    </ul>
+  </div>
+
+  <ul class="flex gap-8 justify-center">
+    <li class="font-thin"><i class="fa-solid fa-hourglass-half text-primary text-lg"></i> 15 minutes</li>
+    <li class="font-thin"><i class="fa-solid fa-dumbbell text-primary text-lg"></i> facile</li>
+    <li class="font-thin"><i class="fa-solid fa-piggy-bank text-primary text-lg"></i> 15 minutes</li>
+  </ul>
+
+  <div class="w-3/4 mx-auto">
+    <div class="flex gap-4 mx-auto items-center">
+      <hr class="bg-primary w-full h-1">
+      <h2 class="text-lg font-bold">Ingrédients</h2>
+      <hr class="bg-primary w-full h-1">
+    </div>
+    <ul class="grid grid-cols-4 gap-8 w-4/5 mx-auto">
+      <li class="flex flex-col items-center">
+        <img src="https://picsum.photos/201" class="rounded-lg shadow-xl" />
+        <span class="font-bold mt-2">120g</span>
+        <span>Farine</span>
+      </li>
+      <li class="flex flex-col items-center">
+        <img src="https://picsum.photos/202" class="rounded-lg shadow-xl" />
+        <span class="font-bold mt-2">120g</span>
+        <span>Farine</span>
+      </li>
+      <li class="flex flex-col items-center">
+        <img src="https://picsum.photos/203" class="rounded-lg shadow-xl" />
+        <span class="font-bold mt-2">120g</span>
+        <span>Farine</span>
+      </li>
+      <li class="flex flex-col items-center">
+        <img src="https://picsum.photos/204" class="rounded-lg shadow-xl" />
+        <span class="font-bold mt-2">120g</span>
+        <span>Farine</span>
+      </li>
+      <li class="flex flex-col items-center">
+        <img src="https://picsum.photos/205" class="rounded-lg shadow-xl" />
+        <span class="font-bold mt-2">120g</span>
+        <span>Farine</span>
+      </li>
+      <li class="flex flex-col items-center">
+        <img src="https://picsum.photos/206" class="rounded-lg shadow-xl" />
+        <span class="font-bold mt-2">120g</span>
+        <span>Farine</span>
+      </li>
+      <li class="flex flex-col items-center">
+        <img src="https://picsum.photos/207" class="rounded-lg shadow-xl" />
+        <span class="font-bold mt-2">120g</span>
+        <span>Farine</span>
+      </li>
+    </ul>
+  </div>
+
+  <div class="w-3/4 mx-auto">
+    <div class="flex gap-4 mx-auto items-center">
+      <hr class="bg-primary w-full h-1">
+      <h2 class="text-lg font-bold">Préparation</h2>
+      <hr class="bg-primary w-full h-1">
+    </div>
+
+    <ul class="flex flex-col gap-8 my-8">
+      <li>
+        <span class="text-5xl font-bold">1</span>
+        <p>Réchauffez les blinis selon l’indication du paquet. Etalez-les légèrement à l’aide d’un rouleau à pâtisserie.</p>
+      </li>
+      <li>
+        <span class="text-5xl font-bold">2</span>
+        <p>Réchauffez les blinis selon l’indication du paquet. Etalez-les légèrement à l’aide d’un rouleau à pâtisserie.</p>
+      </li>
+      <li>
+        <span class="text-5xl font-bold">3</span>
+        <p>Réchauffez les blinis selon l’indication du paquet. Etalez-les légèrement à l’aide d’un rouleau à pâtisserie.</p>
+      </li>
+    </ul>
+  </div>
+  <ul class="w-3/4 mx-auto flex gap-4 font-bold justify-end">
+    <li>Source : </li>
+    <li><a class="underline">Biodélice</a></li>
+    <li><a class="underline">Marmiton</a></li>
+  </ul>
+
+</div>
