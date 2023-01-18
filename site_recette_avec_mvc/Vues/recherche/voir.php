b175d72fef8c9e0983d033e3a3b7138b942a56ea
Ismaiil23 <115786042+Ismaiil23@users.noreply.github.com>
2023-01-18 16:56:20 +0100

    Delete site_recette_avec_mvc/Vues/recherche directory

--- site_recette_avec_mvc/Vues/recherche/voir.php
+++
@@ -1,6 +0,0 @@
- <!-- Formulaire de recherche -->
- <form method="get" action="index.php?url=Recherche/searchRecipes">
-    <input type="text" name="query" placeholder="Rechercher une recette">
-    <input type="submit" value="Rechercher">
-</form>
-
