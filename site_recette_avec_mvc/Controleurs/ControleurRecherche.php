621f0072eec8987d612f83e546e1dd6b9c3cebd7
Ismaiil23 <115786042+Ismaiil23@users.noreply.github.com>
2023-01-18 16:59:10 +0100

    recherche update

--- site_recette_avec_mvc/Controleurs/ControleurRecherche.php
+++ site_recette_avec_mvc/Controleurs/ControleurRecherche.php
@@ -1,36 +1,11 @@
 <?php
-
 final class ControleurRecherche
 {
     public function defautAction()
     {
         $O_recherche =  new Recherche();
-        Vue::montrer('recherche/voir', array('Recherche' =>  $O_recherche->donneMessage()));
+        $query = $_GET['query'];
+        Vue::montrer('defaut/voir', array('recherche' =>  $O_recherche->searchRecipes($query)));
 
-    }
-
-        //Fonction de recherche des recettes
-    public function searchRecipes($query) {
-        $host = 'mysql-thesavorist.alwaysdata.net  ';
-        $dbname = 'thesavorist_site';
-        $username = '295285';
-        $password = '*OnadesnotesIncr13*';
-
-        $db = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-        $stmt = $this->$db->prepare("SELECT id, nom FROM recettes WHERE nom LIKE '%$query%'");
-        $stmt->execute();
-        return $stmt->fetchAll();
-    }
-
-
-}
-
-$recherche = new Recherche();
-if(isset($_GET['query'])){
-    $query = $_GET['query'];
-    $recipes = $recherche->searchRecipes($query);
-    foreach ($recipes as $recipe) {
-        echo '<li><a href="recipe.php?id=' . $recipe['id'] . '">' . $recipe['nom'] . '</a></li>';
     }
 }
-?>
