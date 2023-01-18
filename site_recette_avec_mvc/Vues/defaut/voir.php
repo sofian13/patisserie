58d56d5a4bf9f28a55b001ca265a7891f6571bb4
Ismaiil23 <mkisma84@gmail.com>
2023-01-18 17:28:04 +0100

    recherche up

--- site_recette_avec_mvc/Vues/defaut/voir.php
+++ site_recette_avec_mvc/Vues/defaut/voir.php
@@ -1,10 +1,11 @@
- <!-- Formulaire de recherche -->
+<div class="formRecherche">
+<!-- Formulaire de recherche -->
  <form method="get" action="index.php?url=Recherche" id = "form">
     <input type="text" name="query" placeholder="Rechercher une recette">
     <input type="text" name="url" value="Recherche" id="url">
     <input type="submit" value="Rechercher" form ="form">
 </form>
-
+</div>
 
 
 <div class="divInput">
@@ -15,48 +16,30 @@
 <?php
 
 if(isset($_GET['query'])){
-    foreach ($A_vue['recherche'] as $recipe) {
-      echo '<div class = "card"><div class = "card__image-holder"><img class = "card__image" src ="data:image/jpeg;base64,'.base64_encode($recipe['image']).'" alt = "wave"/></div>
-      <div class = "card-title">
-        <a href = "#" class="toggle-info btn">
-          <span class="left"></span>
-          <span class="right"></span>
-        </a>
-        <h2>' .$recipe['nom'] .'</h2>
-      </div>
-      <div class ="card-flap flap-1">
-        <div class="card-description">' . $recipe['liste_particularite'] . '</div>
-        <div class = "card-flap flap2">
-          <div class = "card-actions">
-              <a href ="#" class="btn">Read more</a>
-          </div>
-        </div>
-        </div>
-      </div>';       
-     }
+   $recipes = $A_vue['recherche'];
 }
-
 else {
-    foreach ($A_vue['recipes'] as $recipe) {
-        echo '<div class = "card"><div class = "card__image-holder"><img class = "card__image" src ="data:image/jpeg;base64,'.base64_encode($recipe['image']).'" alt = "wave"/></div>
-  <div class = "card-title">
-    <a href = "#" class="toggle-info btn">
-      <span class="left"></span>
-      <span class="right"></span>
-    </a>
-    <h2>' .$recipe['nom'] .'</h2>
+  $recipes = $A_vue['recipes'];
+  }
+
+    foreach ($recipes as $recipe) {
+      echo '<div class = "card"><div class = "card__image-holder"><img class = "card__image" src ="data:image/jpeg;base64,'.base64_encode($recipe['image']).'" alt = "wave"/></div>
+<div class = "card-title">
+  <a href = "#" class="toggle-info btn">
+    <span class="left"></span>
+    <span class="right"></span>
+  </a>
+  <h2>' .$recipe['nom'] .'</h2>
+</div>
+<div class ="card-flap flap-1">
+  <div class="card-description">' . $recipe['liste_particularite'] . '</div>
+  <div class = "card-flap flap2">
+    <div class = "card-actions">
+        <a href ="#" class="btn">Read more</a>
+    </div>
   </div>
-  <div class ="card-flap flap-1">
-    <div class="card-description">' . $recipe['liste_particularite'] . '</div>
-    <div class = "card-flap flap2">
-      <div class = "card-actions">
-          <a href ="#" class="btn">Read more</a>
-      </div>
-    </div>
-    </div>
-  </div>';
-        //echo '<img src="data:image/jpeg;base64,'.base64_encode($recipe['image']).'"/>';
-    }
+  </div>
+</div>';
   }
     ?>
 </div>
