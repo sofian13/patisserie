9a3d2254af3d3422762618411694e28b6714f002
Sofian Zerkouni <91824277+sofian13@users.noreply.github.com>
2023-01-18 17:28:46 +0100

    Update ControleurRecette.php

--- site_recette_avec_mvc/Controleurs/ControleurRecette.php
+++ site_recette_avec_mvc/Controleurs/ControleurRecette.php
@@ -10,18 +10,22 @@ final class ControleurRecette
     }
 
     public function ajoute_recetteAction(array $urlParameters, array $postParameters) {
+        var_dump($_FILES);
         $nom = $postParameters['nom'];
         $ingredient = $postParameters['ingredient'];
         $instruction = $postParameters['instruction'];
         $image = file_get_contents($_FILES['avatar']['tmp_name']);
+        $particularite = $postParameters['particularite'];
         $liste_ingredient = $postParameters['liste_ingredient'];
         $liste_ustensile = $postParameters['liste_ustensile'];
         $temps = $postParameters['temps'];
         $difficulte = $postParameters['difficulte'];
         $cout = $postParameters['cout'];
 
+
+     
         $O_bdd = new Recette();
 
-        $O_bdd ->ajoute_recette($nom,$ingredient,$instruction,$image, $liste_ingredient,$liste_ustensile,$temps,$difficulte,$cout);
+        $O_bdd ->ajoute_recette($nom,$ingredient,$instruction,$image,$liste_ingredient,$liste_ustensile,$temps,$difficulte,$cout,$particularite);
     }
 }
