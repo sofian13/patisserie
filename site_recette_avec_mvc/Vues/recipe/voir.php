6ac8459566fb6c4eddde070a440c42f2f115da7a
root <antony.cilleros@etu.univ-amu.fr>
2023-01-19 04:39:30 +0000

    template html/css des commentaires pour recipe

--- site_recette_avec_mvc/Vues/recipe/voir.php
+++ site_recette_avec_mvc/Vues/recipe/voir.php
@@ -88,6 +88,43 @@
       </li>
     </ul>
   </div>
+  <!-- Template pour les commentaires -->
+  <div class="w-3/4 mx-auto">
+    <div class="flex gap-4 mx-auto items-center">
+      <hr class="bg-primary w-full h-1">
+      <h2 class="text-lg font-bold">Commentaires</h2>
+      <hr class="bg-primary w-full h-1">
+    </div>
+
+    <ul class="flex flex-col gap-8 my-8">
+      <li class="comm">
+        <div class="row">
+          <div class="column">
+            <img src="https://media.discordapp.net/attachments/885515822817234954/1063397423428403230/pngegg.png?width=581&height=581" id="avatar-preview-comm" alt="avatar">
+          </div>
+          <div class="column">
+            <p class="pseudo">Pseudo</p>
+            <p class="star"><span>★★★★★</span> Titre</p>
+            <p>Je suis un commentaire.</p>
+          </div>
+        </div>
+      </li>
+      <li class="comm">
+        <div class="row">
+          <div class="column">
+            <img src="https://media.discordapp.net/attachments/885515822817234954/1063397423428403230/pngegg.png?width=581&height=581" id="avatar-preview-comm" alt="avatar">
+          </div>
+          <div class="column">
+            <p class="pseudo">RAYTEK</p>
+            <p class="star"><span>★★</span>★★★ Chiant</p>
+            <p>J'ai fait le template html/css comme on peut le voir. Il me faut encore faire le php/sql et adapter la bdd. Donc je suis loin d'avoir fini.</p>
+          </div>
+        </div>
+      </li>
+      
+    </ul>
+  </div>
+  <!-- footer -->
   <ul class="w-3/4 mx-auto flex gap-4 font-bold justify-end">
     <li>Source : </li>
     <li><a class="underline">Biodélice</a></li>
